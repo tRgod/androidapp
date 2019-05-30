3ec6a834d0e51ff8e632c7cda086b7ff5271f45f
LeMinoo <njoer16@student.sdu.dk>
2019-05-30 22:11:13 +0200

    Jeg ved ikke om det her kan pushes lululululu

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,37 +1,78 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button DatabaseBtn;
-    private Button FirebaseBtn;
+
+    private static final String TAG = "MainActivity";
+
+    DatabaseHelper mDatabaseHelper;
+    private Button btnAdd, btnViewData, firebasebtn;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DatabaseBtn = (Button) findViewById(R.id.databaseBtn);
-        FirebaseBtn = (Button) findViewById(R.id.firebaseBtn);
+        firebasebtn = (Button) findViewById(R.id.fireBtn) ;
+        editText = (EditText) findViewById(R.id.ItemName);
+        btnAdd = (Button) findViewById(R.id.AddButton);
+        btnViewData = (Button) findViewById(R.id.ViewDataButton);
+        mDatabaseHelper = new DatabaseHelper(this);
 
-        DatabaseBtn.setOnClickListener(new View.OnClickListener() {
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent1 = new Intent(MainActivity.this, SQLDataBaseActivity.class);
-                    startActivity(intent1);
+                String newEntry = editText.getText().toString();
+                if (editText.length() != 0) {
+                    AddData(newEntry);
+                    editText.setText("");
+                } else {
+                    toastMessage("You must put something in the text field!");
+                }
 
             }
         });
 
-       FirebaseBtn.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Intent i = new Intent(MainActivity.this, FireBaseActivity.class);
-               startActivity(i);
-           }
-       });
+        btnViewData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListDataActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        firebasebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, FireBaseActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+    }
+
+    public void AddData(String newEntry) {
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+
+        if (insertData) {
+            toastMessage("Data Successfully Inserted!");
+        } else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
     }
 }
